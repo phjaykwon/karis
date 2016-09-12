e856bfdfc18379f2950c5fddac422a803679a511
Phil Jay Kwon <phjaykwon93@gmail.com>
2016-09-12 00:00:17 -0700

    transpose added

---
+++ karis/app/assets/javascripts/Events.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+      $(".transpose-up").on("click",function(){
+            $("span.chord").text(function(i,txt) {
+                  $(this).text(ChordsUtils.transposeChord(txt, 1));
+            });
+      });
+
+
+      $(".transpose-down").on("click",function(){
+            $("span.chord").text(function(i,txt){
+                  $(this).text(ChordsUtils.transposeChord(txt, -1));
+            });
+      });
+      
+      $(".transpose-plus").click(function() {
+            $(".main").css("font-size","+=1");
+      });
+
+      $(".transpose-minus").click(function() {
+            $(".main").css("font-size","-=1");
+      });
+});
+
--- karis/app/models/chord.rb
+++ karis/app/models/chord.rb
@@ -5,7 +5,7 @@ class Chord < ApplicationRecord
 	attr_accessor :artist_full_name
 
 	def self.valid_chords
-		["A", "B", "C", "D", "G", "Em"] 
+		["A", "B", "C", "D", "G", "E", "Em", "B/D#", "C#m", "F#m", "Asus7"]  # hardcoded for now.
 	end 
 
 	def self.search(search)
