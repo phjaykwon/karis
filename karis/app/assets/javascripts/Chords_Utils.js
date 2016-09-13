1d5b3334e24fe3e40ffbd5de2a12a0150a50c055
Phil Jay Kwon <phjaykwon93@gmail.com>
2016-09-12 21:07:58 -0700

    create isAChord

--- karis/app/assets/javascripts/Chords_Utils.js
+++ karis/app/assets/javascripts/Chords_Utils.js
@@ -1,4 +1,8 @@
-ChordsUtils = function () {
+;(function(ChordsUtils, $, undefined) {
+
+	var scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+
     var addSpans = function(line, valid_chords) {
     	var new_line = "";
     	var words = line.split(" ");
@@ -7,10 +11,10 @@ ChordsUtils = function () {
 			var new_word = "";
 			if (words[i].length == 0) {
 				new_word = " ";
-			} else if ($.inArray(words[i], valid_chords) != -1) {
-				new_word = "<span class=\"chord\">" +  words[i] + "</span>"
+			} else if (isAChord(words[i], valid_chords)) {
+				new_word = "<span class=\"chord\">" +  words[i] + "</span> "
 			} else {
-				new_word = words[i];
+				new_word = words[i] + " ";
 			}
 			new_line += new_word;
 		}
@@ -25,14 +29,26 @@ ChordsUtils = function () {
 
 		var chord_count = 0;
 		for (var i = 0; i < words.length; i++) {
-			if ($.inArray(words[i], valid_chords) != -1) {
+			if (isAChord(words[i], valid_chords)) {
 				chord_count += 1;
 			}
 		}
 		return chord_count > 0 && chord_count >= Math.floor(words.length/2);
 	}
 
-    this.updateContent = function(text, valid_chords) {
+	var isAChord = function(txt, valid_chords) {
+		var indiv_chords = txt.split("/");
+		if (indiv_chords.length == 1) {
+			return $.inArray(txt, valid_chords) != -1;
+		} else if (indiv_chords.length == 2) {
+			return $.inArray(indiv_chords[0], valid_chords) != -1 && $.inArray(indiv_chords[1], valid_chords) != -1;
+		} else {
+			return false;
+		}
+
+	}
+
+    ChordsUtils.updateContent = function(text, valid_chords) {
     	var lines = text.split("\n");
 		var new_text = "";
 		$.each(lines, function(i, line) {
@@ -44,14 +60,11 @@ ChordsUtils = function () {
 
 		return new_text;
     }
-    this.transposeChord = function(chord, amount) {
-            var scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    ChordsUtils.transposeChord = function(chord, amount) {
             return chord.replace(/[CDEFGAB]#?/g,
                   function(match) {
                         var i = (scale.indexOf(match) + amount) % scale.length;
                         return scale[ i < 0 ? i + scale.length : i ];
                   });
       }
-};
-
-ChordsUtils = new ChordsUtils();
+})(window.ChordsUtils = window.ChordsUtils || {}, jQuery);
