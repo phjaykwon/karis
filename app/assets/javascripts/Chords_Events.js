96ee6fba2fddf813f6f982838b0a8633606d536e
James <jameshong623@gmail.com>
2016-09-29 21:27:38 -0700

    Testing out the chord diagrams.

--- app/assets/javascripts/Chords_Events.js
+++ app/assets/javascripts/Chords_Events.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
             $("pre").css("font-size","-=1");
       });
 
-      $("span:contains("D")").hover(function() {
+      $("span.chord:contains("D")").click(function() {
             $(this).css("color","red");
       });
 
