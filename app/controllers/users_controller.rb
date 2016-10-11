f2c367c436d4e3b750390224f8273f2c3bee0cd5
Eric Jan <erickwanjan@berkeley.edu>
2016-10-10 19:15:20 -0700

    added chord submission firewall

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
 
 
 	def index
-		@users = User.search(params[:search])
+		#@users = User.search(params[:search])
+		#if current_user == null
+		#	redirect_to new_user_session_path
 	end
 
 	def show 	# modify to fit user
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ApplicationRecord
 	has_many :chords
 
 	def full_name
-  		"#{first_name} #{last_name}"
+  		"#{name}"
 	end
 end
