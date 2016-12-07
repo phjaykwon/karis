7515288033cd71d996a12baa317c9a56a32421d8
James <jameshong623@gmail.com>
2016-12-07 15:48:03 -0800

    Dumping all the CSS changes for the form.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   get 'my_chords', to: 'my_chords#index'
 
+  get 'my_account', to: 'my_account#index'
+
+
 	root 'main#index' # routes root to main#index controller action
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
